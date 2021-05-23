0a2055f5ab17bd9d00a13414be66d1057d73e7c5
yosshii50 <kasaubunts@gmail.com>
2021-05-23 11:04:24 +0900

    Update

--- php/code.php
+++ php/code.php
@@ -1,9 +1,22 @@
-<?php
+<html>
+<meta http-equiv="content-type" charset="utf-8">
+        <head>
+                <title>‚»‚Ì‚Ü‚Ü•\Ž¦</title>
+                <link rel="icon" href="/favicon.ico">
+        </head>
+        <body>
+        <pre><code><?php
 
 $adr = $_REQUEST['a'];
-echo $adr;
+//echo '<pre>';
 
-echo file_get_contents( $adr );
-
+$data = file_get_contents( $adr );
+//$data = readfile( $adr );
+$data = str_replace( '<' , '&lt;' , $data );
+$data = str_replace( '>' , '&gt;' , $data );
+echo $data;
 
 ?>
+</code></pre>
+        </body>
+</html>
